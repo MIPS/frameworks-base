c471706ee09d8351d44b60fbd73c48999c9014fe
Chris Dearman <chris@mips.com>
2011-05-09 20:39:11 -0700

    [MIPS] Enable optimised gglFloatToFixed for hardfloat builds
    The assembler code works just as well for hardfloat builds once the
    float value is in an integer register and is significantly faster
    than the generic C code.
    
    (cherry picked from commit fdcc1d8cdfd4f549fb550cfe863282dae275eb66)
    
    Change-Id: I748078353dc6def061f44517dc9eb0c2b7016037

--- opengl/libagl/Android.mk
+++ opengl/libagl/Android.mk
@@ -39,9 +39,7 @@ ifeq ($(ARCH_ARM_HAVE_TLS_REGISTER),true)
 endif
 
 ifeq ($(TARGET_ARCH),mips)
-    ifneq ($(ARCH_MIPS_HAS_FPU),true)
-	LOCAL_SRC_FILES += arch-$(TARGET_ARCH)/fixed_asm.S
-    endif
+    LOCAL_SRC_FILES += arch-$(TARGET_ARCH)/fixed_asm.S
     LOCAL_CFLAGS += -fstrict-aliasing
     # The graphics code can generate division by zero
     LOCAL_CFLAGS += -mno-check-zero-division
