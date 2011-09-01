d0798549e75acdb329db0c585a9a2b55b4edc53e
Chris Dearman <chris@mips.com>
2011-09-01 03:21:17 -0700

    [ENDIAN] Fix various endianess dependencies in media framework
    (cherry picked from commit ed0f80760342631b0e46ac0290143711c4fa037e)
    
    Change-Id: I11614b3221ff49e09a61a10fcb190ba3ec4a275f

--- media/libstagefright/Utils.cpp
+++ media/libstagefright/Utils.cpp
@@ -44,13 +44,12 @@ uint64_t U64LE_AT(const uint8_t *ptr) {
     return ((uint64_t)U32LE_AT(ptr + 4)) << 32 | U32LE_AT(ptr);
 }
 
-// XXX warning: these won't work on big-endian host.
 uint64_t ntoh64(uint64_t x) {
-    return ((uint64_t)ntohl(x & 0xffffffff) << 32) | ntohl(x >> 32);
+    return (uint64_t)ntohq(x);
 }
 
 uint64_t hton64(uint64_t x) {
-    return ((uint64_t)htonl(x & 0xffffffff) << 32) | htonl(x >> 32);
+    return (uint64_t)htonq(x);
 }
 
 }  // namespace android
