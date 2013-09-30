46ad5bd549fc3bef457482f9fd7f7fcabd5f0929
Duane Sand <duane.sand@imgtec.com>
2013-09-30 15:59:07 -0700

    [MC] Add Magiccode support.
    Five dev-mips-jb-mr1 commits squashed into one,
    including Aug 16 "Selectively prefer Arm abi over Mips
    abi for some apks".
    
    Change-Id: I5f5434e6983fe09bbae3ee59e2046c964065ce4b

--- core/java/com/android/internal/content/NativeLibraryHelper.java
+++ core/java/com/android/internal/content/NativeLibraryHelper.java
@@ -16,10 +16,19 @@
 
 package com.android.internal.content;
 
+import android.content.pm.PackageParser;
 import android.os.Build;
 import android.util.Slog;
+import org.xmlpull.v1.XmlPullParser;
+import org.xmlpull.v1.XmlPullParserException;
+import org.xmlpull.v1.XmlPullParserFactory;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+import java.io.IOException;
+
 
 /**
  * Native libraries helper.
@@ -31,23 +40,112 @@ public class NativeLibraryHelper {
 
     private static final boolean DEBUG_NATIVE = false;
 
-    private static native long nativeSumNativeBinaries(String file, String cpuAbi, String cpuAbi2);
+    /** Per-application MagicCode hints for emulating Arm code on MIPS: */
 
+    /** These are static global vars rather than passed-through args, because
+        - Most of the direct callers deal only with filenames, not package class objects.
+        - The filenames are sometimes random temp names without package names.
+        - The upper layer knowing about package names is sometimes in some asynchronous thread.
+     */
+
+    /** Unique name of application in Google Play market, version independent */
+    private static String MCPackageName = "";
+
+    /** Per-app tuning of Build.CPU_ABI, CPU_ABI2, ... */
+    private static String[] MCCpuAbi = {"", "", ""};
+
+    /** Default for Arm apps, whether /proc/cpuinfo should encourage optional use of Neon instrs */
+    public static final boolean MCShowNeonDefault = true;
+
+    /** Per-app tuning of whether /proc/cpuinfo should encourage optional use of Neon instrs */
+    private static boolean MCShowNeon = MCShowNeonDefault;
+
+    /** Save apk package name for per-application tuning of fat apk unpacking */
+    public static void customizeAbiByAppname(String packageName) {
+        MCPackageName = packageName;
+    }
+
+    /** Lookup per-application hints for how to emulate Arm native code on MIPS */
+    private static void lookupCustomizedAbi() {
+
+        /* Defaults for when app is not named in the magiccode_prefs.xml file */
+        MCCpuAbi[0] = Build.CPU_ABI;
+        MCCpuAbi[1] = Build.CPU_ABI2;
+        MCCpuAbi[2] = "";
+        MCShowNeon = MCShowNeonDefault;
+        if (!Build.CPU_ABI.equals("mips"))
+            return;
+
+        MCCpuAbi[1] = "armeabi";  /* temp workaround, until libakim works well on more armv7 apps */
+
+        if (MCCpuAbi[1].equals("armeabi-v7a"))
+            MCCpuAbi[2] = "armeabi";
+        else if (MCCpuAbi[1].equals("armeabi"))
+            MCCpuAbi[2] = "armeabi-v7a";
+
+        if (MCPackageName.isEmpty())
+            return;
+
+        Slog.d(TAG, "Customizing CpuAbi for " + MCPackageName);
+        try {
+            InputStream in = new FileInputStream("/data/system/magiccode_prefs.xml");
+            try {
+                XmlPullParserFactory factory = XmlPullParserFactory.newInstance();
+                factory.setNamespaceAware(true);
+                XmlPullParser xpp = factory.newPullParser();
+                xpp.setInput(in, "UTF-8");
+                int eventType = xpp.getEventType();
+                while (eventType != XmlPullParser.END_DOCUMENT) {
+                    if (eventType == XmlPullParser.START_TAG &&
+                        xpp.getName().equals("apk")            ) {
+                        Slog.d(TAG, "prefs for "+xpp.getAttributeValue(0));
+                        /* TODO: check field names instead of assuming positions */
+                        if (xpp.getAttributeValue(0).equals(MCPackageName) ||
+                            xpp.getAttributeValue(0).equals("*")             ) {
+                            /* AttributeValue(1) version= is now ignored */
+                            MCCpuAbi[0] = xpp.getAttributeValue(2);  /* first=  */
+                            MCCpuAbi[1] = xpp.getAttributeValue(3);  /* second= */ 
+                            MCCpuAbi[2] = xpp.getAttributeValue(4);  /* third=  */
+                            /* features= */
+                            MCShowNeon = xpp.getAttributeValue(5).indexOf("neon") != -1;
+                            Slog.d(TAG, "Customizing CpuAbi to " +
+                                         MCCpuAbi[0] + ", " +
+                                         MCCpuAbi[1] + ", " +
+                                         MCCpuAbi[2] + "; " +
+                                        "neon " + MCShowNeon +
+                                        " for package " + MCPackageName);
+                            break;
+                        }
+                    }
+                    eventType = xpp.next();
+                }
+            } catch (IOException e) {
+                Slog.d(TAG, "Mangled magiccode_prefs.xml file", e);
+            } catch (XmlPullParserException e) {
+                Slog.d(TAG, "Mangled magiccode_prefs.xml file", e);
+            }
+            in.close();
+        } catch (FileNotFoundException e) {
+        } catch (IOException e) {
+        }
+    }
+
+    private static native long nativeSumNativeBinaries(String file, String cpuAbi,
+                                                       String cpuAbi2, String cpuAbi3);
     /**
      * Sums the size of native binaries in an APK.
      *
      * @param apkFile APK file to scan for native libraries
-     * @return size of all native binary files in bytes
+     * @return upper bound on size of native binary files for possible abi's, in bytes
      */
     public static long sumNativeBinariesLI(File apkFile) {
-        final String cpuAbi = Build.CPU_ABI;
-        final String cpuAbi2 = Build.CPU_ABI2;
-        return nativeSumNativeBinaries(apkFile.getPath(), cpuAbi, cpuAbi2);
+        // get default abi1/2/3; don't know packageName for looking up custom abi
+        lookupCustomizedAbi();
+        return nativeSumNativeBinaries(apkFile.getPath(), MCCpuAbi[0], MCCpuAbi[1], MCCpuAbi[2]);
     }
 
     private native static int nativeCopyNativeBinaries(String filePath, String sharedLibraryPath,
-            String cpuAbi, String cpuAbi2);
-
+            String cpuAbi, String cpuAbi2, String cpuAbi3, boolean showNeon);
     /**
      * Copies native binaries to a shared library directory.
      *
@@ -57,10 +155,9 @@ public class NativeLibraryHelper {
      *         error code from that class if not
      */
     public static int copyNativeBinariesIfNeededLI(File apkFile, File sharedLibraryDir) {
-        final String cpuAbi = Build.CPU_ABI;
-        final String cpuAbi2 = Build.CPU_ABI2;
-        return nativeCopyNativeBinaries(apkFile.getPath(), sharedLibraryDir.getPath(), cpuAbi,
-                cpuAbi2);
+        lookupCustomizedAbi();
+        return nativeCopyNativeBinaries(apkFile.getPath(), sharedLibraryDir.getPath(),
+                                        MCCpuAbi[0], MCCpuAbi[1], MCCpuAbi[2], MCShowNeon);
     }
 
     // Convenience method to call removeNativeBinariesFromDirLI(File)
--- services/java/com/android/server/pm/PackageManagerService.java
+++ services/java/com/android/server/pm/PackageManagerService.java
@@ -957,6 +957,7 @@ public class PackageManagerService extends IPackageManager.Stub {
                                     PackageManager.VERIFICATION_ALLOW,
                                     state.getInstallArgs().getUser());
                             try {
+                                // did customizeAbiByAppname() when this pending step was started
                                 ret = args.copyApk(mContainerService, true);
                             } catch (RemoteException e) {
                                 Slog.e(TAG, "Could not contact the ContainerService");
@@ -996,6 +997,7 @@ public class PackageManagerService extends IPackageManager.Stub {
                             broadcastPackageVerified(verificationId, args.packageURI,
                                     response.code, state.getInstallArgs().getUser());
                             try {
+                                // did customizeAbiByAppname() when this pending step was started
                                 ret = args.copyApk(mContainerService, true);
                             } catch (RemoteException e) {
                                 Slog.e(TAG, "Could not contact the ContainerService");
@@ -4564,6 +4566,7 @@ public class PackageManagerService extends IPackageManager.Stub {
                                 + path);
                     }
                 } else {
+                    NativeLibraryHelper.customizeAbiByAppname(pkg.packageName);
                     if (!isForwardLocked(pkg) && !isExternal(pkg)) {
                         /*
                          * Update native library dir if it starts with
@@ -7449,6 +7452,8 @@ public class PackageManagerService extends IPackageManager.Stub {
                     userIdentifier = UserHandle.USER_OWNER;
                 }
 
+                NativeLibraryHelper.customizeAbiByAppname(pkgLite.packageName);
+
                 /*
                  * Determine if we have any installed package verifiers. If we
                  * do, then we'll defer to them to verify the packages.
@@ -7976,6 +7981,7 @@ public class PackageManagerService extends IPackageManager.Stub {
                 nativeLibraryFile.delete();
             }
             try {
+                // did customizeAbiByAppname() before the step calling copyApk()
                 int copyRet = copyNativeLibrariesForInternalApp(codeFile, nativeLibraryFile);
                 if (copyRet != PackageManager.INSTALL_SUCCEEDED) {
                     return copyRet;
@@ -11348,6 +11354,7 @@ public class PackageManagerService extends IPackageManager.Stub {
                                     final File newNativeDir = new File(newNativePath);
 
                                     if (!isForwardLocked(pkg) && !isExternal(pkg)) {
+                                        NativeLibraryHelper.customizeAbiByAppname(pkg.packageName);
                                         NativeLibraryHelper.copyNativeBinariesIfNeededLI(
                                                 new File(newCodePath), newNativeDir);
                                     }
